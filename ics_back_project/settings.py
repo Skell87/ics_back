6fc2e038b71af9d86ac50abba7dc4f0995cbc21d
Skell87 <brandon.heisler@gmail.com>
2024-06-19 09:07:43 -0400

    deploying still

--- ics_back_project/settings.py
+++ ics_back_project/settings.py
@@ -125,7 +125,7 @@ STORAGES = {
 }
 
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:5173','https://ics-back.fly.dev']
+CORS_ALLOWED_ORIGINS = ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:5173','https://ics-back.fly.dev', 'https://packmule.vercel.app']
 
 CORS_ALLOW_METHODS = [
     'GET',
